472e193127e82dd52d795ee59a0e3ef10566dee2
GitHub Action <action@github.com>
2025-07-17 02:54:01 +0000

    Add generated E2E test specs

--- test-generator/end2end/specs/app.spec.js
+++ test-generator/end2end/specs/app.spec.js
@@ -16,21 +16,16 @@ test.describe('My React App', () => {
   });
 
   test('should contain "init commit" text', async ({ page }) => {
-    await expect(page.locator('#root')).toContainText('init commit');
+    await expect(page.locator('#root h3')).toHaveText('init commit');
   });
 
   test('should contain "auto deployment 1" text', async ({ page }) => {
-    await expect(page.locator('#root')).toContainText('auto deployment 1');
+    await expect(page.locator('#root p')).toHaveText('auto deployment 1');
   });
 
   // Negative Test: Check if a non-existent element is not present
   test('should not contain non-existent element', async ({ page }) => {
     const nonExistentElement = await page.$('.non-existent-class');
     expect(nonExistentElement).toBeNull();
-  });
-
-  // Negative Test: Check if the page does not contain unexpected text
-  test('should not contain unexpected text', async ({ page }) => {
-    await expect(page.locator('#root')).not.toContainText('unexpected text');
   });
 });
